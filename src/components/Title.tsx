916356524bb1850b181aebbac448c2998a680d11
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-11-17 21:25:48 +0900

    chore: ESLint, Prettier 설정

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -10,19 +10,14 @@ interface TypingTextDemoProps {
   cursor: boolean;
 }
 
-const TypingTextDemo = ({
-  delay,
-  holdDelay,
-  loop,
-  cursor,
-}: TypingTextDemoProps) => {
+const TypingTextDemo = ({ delay, holdDelay, loop, cursor }: TypingTextDemoProps) => {
   return (
     <TypingText
       key={`${delay}-${holdDelay}-${loop}-${cursor}`}
       delay={delay}
       holdDelay={holdDelay}
       className="whitespace-pre-line break-keep text-[clamp(1.2rem,3.8vw,2.3rem)] leading-[clamp(1.8rem,4vw,2.2rem)] font-semibold text-white sm:text-[clamp(1.4rem,3vw,2.7rem)] sm:leading-[clamp(2rem,3.2vw,2.6rem)] md:text-[clamp(1.8rem,2.3vw,3.25rem)] md:leading-[clamp(2.6rem,3vw,3.6rem)]"
-      text={`안녕하세요,\n한\u00A0줄의\u00A0UI도\u00A0비즈니스\u00A0임팩트를\u00A0줄\u00A0수\u00A0있어야\u00A0한다고\u00A0믿는\n프론트엔드 개발자 김다은입니다`}
+      text={`안녕하세요 ,\n한\u00A0줄의\u00A0UI도\u00A0비즈니스\u00A0임팩트를\u00A0줄\u00A0수\u00A0있어야\u00A0한다고\u00A0믿는\n프론트엔드 개발자 김다은입니다`}
       loop={loop}
     >
       {cursor && (
