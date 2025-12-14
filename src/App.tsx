dfc09ede3814377d1ff56014d610e6d6f98c4308
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-12-14 16:01:45 +0900

    feat: 페이지 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/common/Header";
 import HomePage from "./pages/HomePage";
+import ValueDetailCard from "./pages/ValueDetailCard";
+import ProjectDetailCard from "./pages/ProjectDetailCard";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomePage />
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/values/:slug" element={<ValueDetailCard />} />
+        <Route path="/projects/:slug" element={<ProjectDetailCard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
